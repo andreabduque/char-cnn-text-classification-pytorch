d820cae3070b11fdf758fe1834218015e3dfd702
Andrea Duque <abd@cin.ufpe.br>
2018-12-10 07:07:38 -0300

    feature: add params and storage calculator

--- train.py
+++ train.py
@@ -15,6 +15,29 @@ import torch.optim.lr_scheduler
 from torch.autograd import Variable
 import torch.nn.functional as F
 
+from torchsummary import summary
+import numpy as np
+
+def count_parameters(model):
+    total_param = 0
+    total_param_conv = 0
+    total_param_fc = 0
+    for name, param in model.named_parameters():
+            num_param = np.prod(param.size())
+            if param.dim() > 1:
+                print(name, ':', 'x'.join(str(x) for x in list(param.size())), '=', num_param)                
+            else:
+                print(name, ':', num_param)
+
+            if ('fc' in name ):
+                total_param_fc += num_param
+            else:
+                total_param_conv += num_param
+           
+            total_param += num_param
+
+    return total_param, total_param_conv, total_param_fc
+
 parser = argparse.ArgumentParser(description='Character level CNN text classifier training')
 # data 
 parser.add_argument('--train_path', metavar='DIR',
@@ -98,6 +121,17 @@ def train(train_loader, dev_loader, model, args):
     if args.cuda:
         model = torch.nn.DataParallel(model).cuda()
 #         model = model.cuda()
+
+
+    print()
+    total_param, total_param_conv, total_param_fc = count_parameters(model)    
+
+    print('\nNum of parameters: ')    
+    print('conv:', total_param_conv)    
+    print('fc:', total_param_fc)
+    print('total:', total_param, '\n')
+
+    summary(model, (32, 1024))
 
     model.train()
 
